5188fc5ecb3900674dd9e99aa1e8bed829d71b89
high_byte <63053861+hananbeer@users.noreply.github.com>
2022-12-24 15:00:19 +0200

    Update getSignatures.js
    semantics

--- getSignatures.js
+++ getSignatures.js
@@ -1,9 +1,9 @@
-const regex_solidity_signatures = /63([0-9a-f]{8})1461([0-9a-f]{4})57/ig
+const regex_solidity_selectors = /63([0-9a-f]{8})1461([0-9a-f]{4})57/ig
 
-function getFunctionSignatures(bytecode) {
-	let groups = bytecode.matchAll(regex_solidity_signatures)
-	let signatures = Array.from(groups).map(g => g[1]) // g[2] holds function IP (instruction pointer)
-	return signatures
+function getFunctionSelectors(bytecode) {
+	let groups = bytecode.matchAll(regex_solidity_selectors)
+	let selectors = Array.from(groups).map(g => g[1]) // g[2] holds function IP (instruction pointer)
+	return selectors
 }
 
-module.exports = getFunctionSignatures
+module.exports = getFunctionSelectors
